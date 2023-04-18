fb61d0607c98e2f5794618c58ac8437562ca53a1
martacirer <martacirermorey@gmail.com>
2023-04-18 13:31:34 +0200

    18/04

---
+++ Assets/Idioma.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Idioma : MonoBehaviour
+{
+   
+    public int Ingls = 1;
+    public string[] TextoIngles;
+    public string[] TextoEspañol;
+
+    public Text[] Text;
+
+    void Update()
+    {
+        
+    }
+}
